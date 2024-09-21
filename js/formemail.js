180bc072b9a3f5759d1115443529bc098abcd0a0
yechiqq <yechi_qq@hotmail.com>
2024-09-21 12:51:17 +0800

    Update formemail.js

--- js/formemail.js
+++ js/formemail.js
@@ -9,7 +9,8 @@ $( document ).ready(function() {
             url: "https://script.google.com/macros/s/AKfycbzhuG01T_n_d_bXutazianX86ahWH_qo7S5o2BirXSx7pGUhWvA1kwnV43zngE_zWh_yw/exec",
             data: formData,
             success: function(){},
-            dataType: "json",
+            crossDomain: true,
+            dataType: 'jsonp',
             contentType : "application/json"
           });
       });
